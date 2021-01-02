caa6ca35d5e14e20793edb2dd24552575961ce34
Your Actual Name <Your Actual Email>
2021-01-02 15:46:14 -0500

    working on dropdown now

--- src/Header.js
+++ src/Header.js
@@ -9,16 +9,38 @@ class Header extends React.Component {
         return (
             <div className="Header">
                 <h2 className="headerName">Hot Sauce</h2>
-                <nav>
-                    <div className="mainHeaderBtns">
-                        <button>Home</button>
+                <nav role="select">
+                    
+                    <div className="dropDown">
+                    <button onClick="myFunction()" className="dropbtn">Dropdown</button>
+                    <div id="myDropdown" className="dropdown-content">
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
+                    </div>
+                    </div>
+                        {/* <button>Home</button>
                         <button>Hot Sauce Directory</button>
                         <button>Profile</button>
                         <button>Search</button>
-                        <button>Contact Us</button>
+                        <button>Contact Us</button> */}
+                    
+                        {/* <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">HS Directory</a></li>
+                            <li><a href="#">Profile</a></li>
+                            <li><a href="#">Search</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul> */}
+                        {/* <select onchange="if (this.value) window.location.href = this.value;">
+                            <option value="#">Home</option>
+                            <option value="#">HS Directory</option>
+                            <option value="#">Profile</option>
+                            <option value="#">Search</option>
+                            <option value="#">Contact Us</option>
+                        </select> */}
 
-
-                    </div>
+                    
                 </nav>
             </div>
         )
