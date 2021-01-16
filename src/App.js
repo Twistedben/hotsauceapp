fbb934295d01225600ee0bd4f4f4fc2c11e3da36
Andrew Aramouni <aramouni8@gmail.com>
2021-01-16 15:04:27 -0500

    added firebase

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import Header from './Header'
 // import SearchBar from './SearchBar'
 // import SearchBarResults from './SearchResultsPage'
 import './MainContent.css'
+import firebase from "firebase/app";
+
 
 class App extends React.Component {
+    const firebaseapp = firebase.apps[0]
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 
     render() {
+
         return (
             <div className="testing">
                 <Header />
--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ import '@fortawesome/fontawesome-free/css/all.min.css'; import
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import firebase from "firebase/app";
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAozgAuIA6qczsPTh3VOfA5auQeI7MR9rU",
+    authDomain: "hotsauceapp-56421.firebaseapp.com",
+    projectId: "hotsauceapp-56421",
+    storageBucket: "hotsauceapp-56421.appspot.com",
+    messagingSenderId: "830033657123",
+    appId: "1:830033657123:web:828c7055d79f86fc9a6096",
+    measurementId: "G-1C6B0B469E"
+});
+
+
 
 ReactDOM.render(
     <App />,
